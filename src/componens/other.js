9cd5f670a05ec71a268f5aeff0cee84a68bbd2d5
NicoNeubauer <116970767+NicoNeubauer@users.noreply.github.com>
2022-12-29 21:53:26 +0100

    gggg

--- src/componens/other.js
+++ src/componens/other.js
@@ -1,13 +1,54 @@
 import React from "react";
 import "./content.css";
 import Svg from "./svg.js";
+import "./other.css";
 
 const Other = () => {
   return (
     <section>
-      <p>Other</p>
+      
       <Svg colur="#DF8282" />
+
+      <h2 className="head2">Other</h2>
+      
+      <h3>Photo</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+        consequat. Duis aute irure 
+      </p>
+npp
+      <div className="imgcon1">
+        <img src="img\other\by_portrett.jpg" alt="" />
+        <img src="img\other\marin_lys.png" alt="" />
+      </div>
+
+      <div className="imgcon1">
+        <img src="img\other\marin_lys.png" alt="" />
+        <img src="img\other\flammer.jpg" alt="" />
+      </div>
+
+      <div className="imgcon1">
+        <img src="img\other\skygge_tegning.jpg" alt="" />
+        <img src="img\other\fargeblanding.jpg" alt="" />
+      </div>
+
+      <h3>Video</h3>
+      <p>
+        jeg kan også vise til mine ferdigheter med video opptak og redigering 
+        som jeg har fra en internship i en tysk mediebedrift hvor 
+        jeg har hatt muligheten på å være en del av en profesjonelt 
+        filmproduksjon.
+      </p>
+
+      <div className="vidcon1">
+        <iframe src="https://www.youtube.com/embed/8Mgw8BlILsw" title="Interview Anne DE"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/AykLmoq9JxY" title="Stop mo film" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+
     </section>
+
   );
 };
 export default Other;
