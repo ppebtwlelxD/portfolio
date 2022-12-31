93cfe558c2b0ac63a7d16a683cb84018598278b4
Andre <andreneubauerlol@gmail.com>
2022-12-31 18:23:07 +0100

    Update other.js

--- src/componens/other.js
+++ src/componens/other.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./content.css";
-import Svg from "./svg.js";
 import "./other.css";
 
 const Other = () => {
   return (
     <section>
-
       <h2 className="head2">Other</h2>
-      
+
       <h3>Photo</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-        consequat. Duis aute irure 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure
       </p>
 
       <div className="imgcon1">
@@ -34,19 +32,26 @@ const Other = () => {
 
       <h3>Video</h3>
       <p>
-        jeg kan også vise til mine ferdigheter med video opptak og redigering 
-        som jeg har fra en internship i en tysk mediebedrift hvor 
-        jeg har hatt muligheten på å være en del av en profesjonelt 
-        filmproduksjon.
+        jeg kan også vise til mine ferdigheter med video opptak og redigering
+        som jeg har fra en internship i en tysk mediebedrift hvor jeg har hatt
+        muligheten på å være en del av en profesjonelt filmproduksjon.
       </p>
 
       <div className="vidcon1">
-        <iframe src="https://www.youtube.com/embed/8Mgw8BlILsw" title="Interview Anne DE"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <iframe src="https://www.youtube.com/embed/AykLmoq9JxY" title="Stop mo film" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/8Mgw8BlILsw"
+          title="Interview Anne DE"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/AykLmoq9JxY"
+          title="Stop mo film"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
-
     </section>
-
   );
 };
 export default Other;
