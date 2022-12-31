a133c79faa2572112e6fb348bcb8c922a16f6ae0
Andre <andreneubauerlol@gmail.com>
2022-12-31 18:21:58 +0100

    d

--- src/componens/main.js
+++ src/componens/main.js
@@ -76,29 +76,84 @@ const Main = () => {
           </button>
         </div>
       </header>
-      
+
       <div className="conditional-render-section">
         {button.design ? <Design /> : <></>}
         {button.code ? <Code /> : <></>}
         {button.other ? <Other /> : <></>}
         {button.abuteMe ? <AbuteMe /> : <></>}
       </div>
+      <div className="buttondivbottom">
+        {button.design ? (
+          <></>
+        ) : (
+          <button
+            style={{ backgroundColor: "#fff4bf" }}
+            id="design"
+            className={button.content}
+            onClick={handleClick}
+          >
+            Design
+          </button>
+        )}
+        {button.code ? (
+          <></>
+        ) : (
+          <button
+            style={{ backgroundColor: "#92beff" }}
+            id="code"
+            className={button.content}
+            onClick={handleClick}
+          >
+            Code
+          </button>
+        )}
+        {button.other ? (
+          <></>
+        ) : (
+          <button
+            style={{ backgroundColor: "#DF8282" }}
+            id="other"
+            className={button.content}
+            onClick={handleClick}
+          >
+            Other
+          </button>
+        )}
+      </div>
 
       <footer>
-        <div className="socals"></div>
-        <div className="links"></div>
-        <div id="contact">
+        <div className="contact">
           <h3>Contact</h3>
-          <a href="tel:+47 98198978">98198978</a>
-          <br />
-          <button id="abuteMe" className={button.content} onClick={handleClick}>
-            Abute Me
-          </button>
-          <a href="niconeubauer.me@gmail.com">niconeubauer.me@gmail.com</a>
-          <br />
-          <br />
-          <p>Made by Nico N.</p>
+          <a>
+            <img src="img/footer/discord.png" alt="" />
+          </a>
+          <a>
+            <img src="img/footer/linktin.png" alt="" />
+          </a>
+          <a>
+            <img src="img/footer/email.png" alt="" />
+          </a>
+          <a>
+            <img src="img/footer/call.png" alt="" />
+          </a>
         </div>
+        <div className="abuteMeFooter">
+          <h3 id="abuteMe" className={button.content} onClick={handleClick}>
+            Abute Me
+          </h3>
+          <a>
+            <img src="img/footer/cv.png" alt="" />
+          </a>
+          <a>
+            <img src="img/footer/user.png" alt="" />
+          </a>
+          <a>
+            <img src="img/footer/github.png" alt="" />
+          </a>
+        </div>
+
+        <p className="madeBy">Made by Nico N. whit react</p>
       </footer>
     </>
   );
