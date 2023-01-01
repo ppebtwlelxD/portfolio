830c88ac632a895455153cf3ba21212a434359cc
Andre <andreneubauerlol@gmail.com>
2023-01-01 17:49:55 +0100

    d

--- src/componens/design.js
+++ src/componens/design.js
@@ -1,12 +1,182 @@
 import React from "react";
 import "./content.css";
-import Svg from "./svg.js";
+import "./design.css";
 
 const Design = () => {
   return (
     <section>
-      <p>Design</p>
-      <Svg colur="#FFF4BF" />
+      <h2 className="head2design">Design</h2>
+      <div className="projekt">
+        <h3>Reko Ringen</h3>
+        <p>
+          Forslag til løsning av nytt REKO system for håndtering av salg. Dette
+          er en rapport som tar for seg prosessen av å konseptualisere, samt
+          designe en ny løsning for Reko, og veien og valgene som ble tatt av
+          gruppen for å lage vår prototype for gruppeprosjektet. I beskrivelsen
+          av oppgaven står det at “Gruppen skal konseptualisere og designe et
+          forslag til en ny løsning for direktesalg av lokale produkter basert
+          på REKO-modellen. Det skal brukes prinsipper for brukersentrert
+          systemutvikling og informasjonsarkitektur” Oppgaven ble derfor tolket
+          som at gruppen skulle finne en bedre løsning enn det som brukes i dag
+          hos matprodusentene og forbrukere. At REKO-ringen brukes i dag for at
+          forbrukere skal kunne handle direkte mot matprodusenter uten noen
+          mellomledd som forhandlere.I denne oppgaven/rapporten skal det
+          presenteres en løsning som vil gjøre det enklere både for
+          matprodusenter og forbrukere. Målet er å gjøre det brukervennlig og
+          enkelt for begge parter, men samtidig å få flere brukere inn på selve
+          REKO prosjektet.
+        </p>
+        <a
+          className="abutton"
+          href="https://www.figma.com/file/ks542VO5FqXJ8knXNEBCK9/Reko2?t=WNH7qrtcoLZLh8Wz-1"
+        >
+          vis i figma
+        </a>
+        <div className="mobileShow">
+          <img src="img\design\rekomobil1.png" alt="" />
+          <img src="img\design\rekomobil2.png" alt="" />
+          <img src="img\design\rekomobil3.png" alt="" />
+          <img src="img\design\rekomobil4.png" alt="" />
+          <img src="img\design\rekomobil5.png" alt="" />
+        </div>
+        <div className="desktopShow">
+          <img src="img\design\rekodesktop1.png" alt="" />
+          <img src="img\design\rekodesktop2.png" alt="" />
+        </div>
+        <div className="designManual">
+          <div className="logoBruk">
+            <h3>Logo</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure{" "}
+            </p>
+            <img src="img\design\rekologo1.png" alt="" />
+            <img src="img\design\rekologo2.png" alt="" />
+          </div>
+          <div className="fargeBruk">
+            <h3>Fargebruk</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure{" "}
+            </p>
+            <img src="img\design\rekocolur.png" alt="" />
+          </div>
+          <div className="fontbruk">
+            <h3>Font</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure{" "}
+            </p>
+            <span className="forntTitel">Roboto</span>
+            <img src="img\design\rekofont.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="projekt">
+        <h3>Work on it</h3>
+        <p>
+          Forslag til løsning av nytt REKO system for håndtering av salg. Dette
+          er en rapport som tar for seg prosessen av å konseptualisere, samt
+          designe en ny løsning for Reko, og veien og valgene som ble tatt av
+          gruppen for å lage vår prototype for gruppeprosjektet. I beskrivelsen
+          av oppgaven står det at “Gruppen skal konseptualisere og designe et
+          forslag til en ny løsning for direktesalg av lokale produkter basert
+          på REKO-modellen. Det skal brukes prinsipper for brukersentrert
+          systemutvikling og informasjonsarkitektur” Oppgaven ble derfor tolket
+          som at gruppen skulle finne en bedre løsning enn det som brukes i dag
+          hos matprodusentene og forbrukere. At REKO-ringen brukes i dag for at
+          forbrukere skal kunne handle direkte mot matprodusenter uten noen
+          mellomledd som forhandlere.I denne oppgaven/rapporten skal det
+          presenteres en løsning som vil gjøre det enklere både for
+          matprodusenter og forbrukere. Målet er å gjøre det brukervennlig og
+          enkelt for begge parter, men samtidig å få flere brukere inn på selve
+          REKO prosjektet.
+        </p>
+        <a
+          className="abutton"
+          href="https://www.figma.com/file/2be3ae5SSYkcbOpEgAsWfK/Nico-Workout-Prototype?node-id=0%3A1&t=cbUq4DwCbkCbXW8T-1"
+        >
+          vis i figma
+        </a>
+        <div className="mobileShow">
+          <img src="img\design\workonitmobil1.png" alt="" />
+          <img src="img\design\workonitmobil2.png" alt="" />
+          <img src="img\design\workonitmobil3.png" alt="" />
+          <img src="img\design\workonitmobil4.png" alt="" />
+          <img src="img\design\workonitmobil5.png" alt="" />
+        </div>
+        <div className="designManual">
+          <div className="logoBruk">
+            <h3>Logo</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure{" "}
+            </p>
+            <img
+              src="img\design\workonitlogo.png"
+              className="workonitlogo"
+              alt=""
+            />
+          </div>
+          <div className="fargeBruk">
+            <h3>Fargebruk</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure{" "}
+            </p>
+            <img
+              src="img\design\workonitcolur.png"
+              className="workonitcolur"
+              alt=""
+            />
+          </div>
+          <div className="fontbruk">
+            <h3>Font</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure{" "}
+            </p>
+            <span className="forntTitel">Inter</span>
+            <img src="img\design\workonitfont.png" alt="" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
