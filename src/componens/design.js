e55b3dfc83c3cf712e5fb383bda6a5d3f324d076
Shabdabhab <59998596+Shabdabhab@users.noreply.github.com>
2023-01-02 13:51:42 +0100

    Update design.js

--- src/componens/design.js
+++ src/componens/design.js
@@ -9,27 +9,10 @@ const Design = () => {
       <div className="projekt">
         <h3>Reko Ringen</h3>
         <p>
-          Forslag til løsning av nytt REKO system for håndtering av salg. Dette
-          er en rapport som tar for seg prosessen av å konseptualisere, samt
-          designe en ny løsning for Reko, og veien og valgene som ble tatt av
-          gruppen for å lage vår prototype for gruppeprosjektet. I beskrivelsen
-          av oppgaven står det at “Gruppen skal konseptualisere og designe et
-          forslag til en ny løsning for direktesalg av lokale produkter basert
-          på REKO-modellen. Det skal brukes prinsipper for brukersentrert
-          systemutvikling og informasjonsarkitektur” Oppgaven ble derfor tolket
-          som at gruppen skulle finne en bedre løsning enn det som brukes i dag
-          hos matprodusentene og forbrukere. At REKO-ringen brukes i dag for at
-          forbrukere skal kunne handle direkte mot matprodusenter uten noen
-          mellomledd som forhandlere.I denne oppgaven/rapporten skal det
-          presenteres en løsning som vil gjøre det enklere både for
-          matprodusenter og forbrukere. Målet er å gjøre det brukervennlig og
-          enkelt for begge parter, men samtidig å få flere brukere inn på selve
-          REKO prosjektet.
+          Forslag til løsning av nytt REKO system for håndtering av salg. Dette er en rapport som tar for seg prosessen av å konseptualisere, samt designe en ny løsning for Reko, og veien og valgene som ble tatt av gruppen for å lage vår prototype for gruppeprosjektet. I beskrivelsen av oppgaven står det at “Gruppen skal konseptualisere og designe et forslag til en ny løsning for direktesalg av lokale produkter basert på REKO-modellen. Det skal brukes prinsipper for brukersentrert systemutvikling og informasjonsarkitektur” Oppgaven ble derfor tolket som at gruppen skulle finne en bedre løsning enn det som brukes i dag hos matprodusentene og forbrukere. At REKO-ringen brukes i dag for at forbrukere skal kunne handle direkte mot matprodusenter uten noen mellomledd som forhandlere.I denne oppgaven/rapporten skal det presenteres en løsning som vil gjøre det enklere både for matprodusenter og forbrukere. Målet er å gjøre det brukervennlig og enkelt for begge parter, men samtidig å få flere
+          brukere inn på selve REKO prosjektet.
         </p>
-        <a
-          className="abutton"
-          href="https://www.figma.com/file/ks542VO5FqXJ8knXNEBCK9/Reko2?t=WNH7qrtcoLZLh8Wz-1"
-        >
+        <a className="abutton" href="https://www.figma.com/file/ks542VO5FqXJ8knXNEBCK9/Reko2?t=WNH7qrtcoLZLh8Wz-1">
           Vis i figma
         </a>
         <div className="mobileShow">
@@ -46,40 +29,18 @@ const Design = () => {
         <div className="designManual">
           <div className="logoBruk">
             <h3>Logo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure{" "}
-            </p>
+            <p>I logo designet for dette prosjektet prøvde jeg å gjenspeile nærheten til kunden som er så viktig for at reko systemet funker. Dette gjorde jeg med bruk av organiske former og bruk av signalfarger grønn. Enkelheten til denne logoen hjelper med å være gjenkjennelig og lett å huske, og enkel å bruke i mange sammenhenger.</p>
             <img src="img\design\rekologo1.png" alt="" />
             <img src="img\design\rekologo2.png" alt="" />
           </div>
           <div className="fargeBruk">
             <h3>Fargebruk</h3>
-            <p>
-              I den grafiske profilen av reko er gråfarger mest dominante med
-              reko grønn mest brukt som sigan farge på knapper og andre
-              handlinger. Dette er fordi reko grønnfargen har høy kontrast mot
-              de andre gråfargene som er brukt i den grafiske designen og derfor
-              skaper den en blikkfang Reko grønn er grønn farge som gjengår i
-              alle sider av reko og også i logen. Det er en farge som skal
-              signaliser den økologiske og vennlige naturen av reko.
-            </p>
+            <p>I den grafiske profilen av reko er gråfarger mest dominante med reko grønn mest brukt som sigan farge på knapper og andre handlinger. Dette er fordi reko grønnfargen har høy kontrast mot de andre gråfargene som er brukt i den grafiske designen og derfor skaper den en blikkfang Reko grønn er grønn farge som gjengår i alle sider av reko og også i logen. Det er en farge som skal signaliser den økologiske og vennlige naturen av reko.</p>
             <img src="img\design\rekocolur.png" alt="" />
           </div>
           <div className="fontbruk">
             <h3>Font</h3>
-            <p>
-              Fronten som er brukt i den grafiske profilen av reko er heter
-              Roboto. Hvor Roboto regula blir brukt for brødtekst og bold kan
-              bli brukt for overskrifter og hoved knapper. Roboto blir brukes
-              for å gjøre at reko virker moderne og ryddig.
-            </p>
+            <p>Fronten som er brukt i den grafiske profilen av reko er heter Roboto. Hvor Roboto regula blir brukt for brødtekst og bold kan bli brukt for overskrifter og hoved knapper. Roboto blir brukes for å gjøre at reko virker moderne og ryddig.</p>
             <span className="forntTitel">Roboto</span>
             <img src="img\design\rekofont.png" alt="" />
           </div>
@@ -88,27 +49,10 @@ const Design = () => {
       <div className="projekt">
         <h3>Work on it</h3>
         <p>
-          Forslag til løsning av nytt REKO system for håndtering av salg. Dette
-          er en rapport som tar for seg prosessen av å konseptualisere, samt
-          designe en ny løsning for Reko, og veien og valgene som ble tatt av
-          gruppen for å lage vår prototype for gruppeprosjektet. I beskrivelsen
-          av oppgaven står det at “Gruppen skal konseptualisere og designe et
-          forslag til en ny løsning for direktesalg av lokale produkter basert
-          på REKO-modellen. Det skal brukes prinsipper for brukersentrert
-          systemutvikling og informasjonsarkitektur” Oppgaven ble derfor tolket
-          som at gruppen skulle finne en bedre løsning enn det som brukes i dag
-          hos matprodusentene og forbrukere. At REKO-ringen brukes i dag for at
-          forbrukere skal kunne handle direkte mot matprodusenter uten noen
-          mellomledd som forhandlere.I denne oppgaven/rapporten skal det
-          presenteres en løsning som vil gjøre det enklere både for
-          matprodusenter og forbrukere. Målet er å gjøre det brukervennlig og
-          enkelt for begge parter, men samtidig å få flere brukere inn på selve
-          REKO prosjektet.
+          Forslag til løsning av nytt REKO system for håndtering av salg. Dette er en rapport som tar for seg prosessen av å konseptualisere, samt designe en ny løsning for Reko, og veien og valgene som ble tatt av gruppen for å lage vår prototype for gruppeprosjektet. I beskrivelsen av oppgaven står det at “Gruppen skal konseptualisere og designe et forslag til en ny løsning for direktesalg av lokale produkter basert på REKO-modellen. Det skal brukes prinsipper for brukersentrert systemutvikling og informasjonsarkitektur” Oppgaven ble derfor tolket som at gruppen skulle finne en bedre løsning enn det som brukes i dag hos matprodusentene og forbrukere. At REKO-ringen brukes i dag for at forbrukere skal kunne handle direkte mot matprodusenter uten noen mellomledd som forhandlere.I denne oppgaven/rapporten skal det presenteres en løsning som vil gjøre det enklere både for matprodusenter og forbrukere. Målet er å gjøre det brukervennlig og enkelt for begge parter, men samtidig å få flere
+          brukere inn på selve REKO prosjektet.
         </p>
-        <a
-          className="abutton"
-          href="https://www.figma.com/file/2be3ae5SSYkcbOpEgAsWfK/Nico-Workout-Prototype?node-id=0%3A1&t=cbUq4DwCbkCbXW8T-1"
-        >
+        <a className="abutton" href="https://www.figma.com/file/2be3ae5SSYkcbOpEgAsWfK/Nico-Workout-Prototype?node-id=0%3A1&t=cbUq4DwCbkCbXW8T-1">
           Vis i figma
         </a>
         <div className="mobileShow">
@@ -121,52 +65,17 @@ const Design = () => {
         <div className="designManual">
           <div className="logoBruk">
             <h3>Logo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure{" "}
-            </p>
-            <img
-              src="img\design\workonitlogo.png"
-              className="workonitlogo"
-              alt=""
-            />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure </p>
+            <img src="img\design\workonitlogo.png" className="workonitlogo" alt="" />
           </div>
           <div className="fargeBruk">
             <h3>Fargebruk</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure{" "}
-            </p>
-            <img
-              src="img\design\workonitcolur.png"
-              className="workonitcolur"
-              alt=""
-            />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure </p>
+            <img src="img\design\workonitcolur.png" className="workonitcolur" alt="" />
           </div>
           <div className="fontbruk">
             <h3>Font</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure{" "}
-            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure </p>
             <span className="forntTitel">Inter</span>
             <img src="img\design\workonitfont.png" alt="" />
           </div>
